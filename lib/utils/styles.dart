81f69ee257994deb2df3e9c08b5aeb7f8b77b4a3
Adil Basar <descrout@gmail.com>
2020-11-07 04:21:31 +0300

    group reusable styles into one file

---
+++ lib/utils/styles.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static final defaultStyle = TextStyle(color: Colors.grey[900]);
+
+  static final h1 = defaultStyle.copyWith(
+    fontWeight: FontWeight.w700,
+    fontSize: 18.0,
+    height: 22 / 18,
+  );
+
+  static final p = defaultStyle.copyWith(
+    fontSize: 16.0,
+  );
+
+  static final error = defaultStyle.copyWith(
+    fontWeight: FontWeight.w500,
+    fontSize: 14.0,
+    color: Colors.red,
+  );
+
+  static final input = InputDecoration(
+    fillColor: Colors.white,
+    focusColor: Colors.grey[900],
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.blue,
+        width: 2.0,
+      ),
+    ),
+    border: OutlineInputBorder(
+      gapPadding: 1.0,
+      borderSide: BorderSide(
+        color: Colors.grey[600],
+        width: 1.0,
+      ),
+    ),
+    hintStyle: TextStyle(
+      color: Colors.grey[600],
+    ),
+  );
+}
