c8ef6bafe9bdae06b38f763cd67e8bad776fef96
Adil Basar <descrout@gmail.com>
2020-11-08 23:36:17 +0300

    updates models

--- lib/models/role.dart
+++ lib/models/role.dart
@@ -1,9 +1,22 @@
+import 'package:flutter/material.dart';
+
 class Role {
   final int id;
   final String name;
   final int code;
 
   Role({this.id, this.name, this.code});
+
+  Color get color {
+    switch (id) {
+      case 1:
+        return Colors.orangeAccent;
+      case 2:
+        return Colors.orange;
+      case 3:
+        return Colors.deepOrange;
+    }
+  }
 
   Role.fromJson(Map<String, dynamic> parsed)
       : id = parsed['id'],
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,19 +1,34 @@
+import 'package:flutter_blog/globals.dart';
 import 'package:flutter_blog/models/role.dart';
+import 'package:flutter_blog/utils/time.dart';
 
 class User {
   final int id;
   final String name;
   final String email;
   final String image;
+  final UnixTime createdAt;
+  final int karma;
   final Role role;
 
-  User({this.id, this.name, this.email, this.image, this.role});
+  User(
+      {this.id,
+      this.name,
+      this.email,
+      this.image,
+      this.createdAt,
+      this.karma,
+      this.role});
+
+  String get getImageURL => 'http://${Globals.SERVER}${image}';
 
   User.fromJson(Map<String, dynamic> parsed)
       : id = parsed['id'],
         name = parsed['name'],
         email = parsed['email'],
         image = parsed['image'],
+        createdAt = UnixTime(parsed['created_at']),
+        karma = parsed['karma'],
         role = Role.fromJson(parsed['role']);
 
   Map<String, dynamic> toJson() => {
@@ -21,6 +36,7 @@ class User {
         'name': name,
         'email': email,
         'image': image,
+        'created_at': (createdAt.sinceEpoch ?? 0) / 1000,
         'role': role.toJson(),
       };
 }
